4fe087bb8d268aed27d2b9fe4e43aa747ecae2de
Antoil <alinciano113@gmail.com>
2024-09-23 11:49:44 +0200

    esercizio quiz
    esercizio in javascript in cui creiamo un quiz di 7 domande

---
+++ static/js/quiz.js
@@ -0,0 +1,32 @@
+let counterAns = 0
+let quizAns = ['roma', 'tevere', 'bianco', 'po', 'bari', 'milano', 'egitto']
+let nameAns = []
+let uncorrect = []
+let percentualValue = 0
+let firstName = null
+while( firstName  === null ){
+    firstName = prompt("Inserisci il tuo nome: ")
+}
+console.log("Benvenuto " + firstName)
+
+nameAns[0]= prompt("Prima domanda: Qual e' la capitale d'Italia?")
+nameAns[1]= prompt("Seconda domanda: Qual e' il fiume che attraversa Roma?")
+nameAns[2]= prompt("Terza domanda: Qual e' il colore della neve?")
+nameAns[3]= prompt("Quarta domanda: Qual e' fiume piu' lungo d'Italia?")
+nameAns[4]= prompt("Quinta domanda: Qual e' il capoluogo della Puglia?")
+nameAns[5]= prompt("Sesta domanda: Qual e' il capoluogo della Lombardia?")
+nameAns[6]= prompt("Settima domanda: Dove si trova la Sfinge?")
+
+for  (let i = 0; i < 7; i++) { 
+    nameAns[i] = nameAns[i].toLowerCase().trim()
+    if(nameAns[i] === quizAns[i]){
+        counterAns++
+    }
+}
+
+if(counterAns > 0) {
+    percentualValue = (counterAns / 7) * 100
+}
+
+alert("Risposte corrette: %" + percentualValue)
+
---
+++ static/js/script.js
@@ -0,0 +1,17 @@
+// let  "parola chiave" dichiarale variabili 
+let firstName = prompt("Inserisci il tuo nome")
+// prompt è una funzione che permette di inserire un input dall'utente 
+/* -restituisce un valore ->  lo possiamo assegnare a una variabile
+   -di default restituisce una string
+*/
+
+alert("Benvenuto " + firstName)
+// mostraun popup nella finestra del browser
+
+console.log("Benvenuto " + firstName)
+// console.log() è una funzione che stampa un messaggio nella console del browser
+
+
+if(firstName === null) {
+    
+}
